49ea48096a0bb44a9ec3934b62c9ca26c5787771
CactusPuppy <CactusPuppy@users.noreply.github.com>
2019-06-28 17:27:27 -0700

    Add main file

---
+++ src/main/java/usa/cactuspuppy/mcmurder/Main.java
@@ -0,0 +1,15 @@
+package usa.cactuspuppy.mcmurder;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Getter
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        long start = System.nanoTime();
+        instance = this;
+    }
+}
