df34628881acbfaef5450c6ac3aa083e1352bcb5
CactusPuppy <CactusPuppy@users.noreply.github.com>
2019-07-11 15:09:24 -0700

    Updated Mansion Murder to match parent abstract

--- src/main/java/com/github/cactuspuppy/mcmurder/game/MansionMurder.java
+++ src/main/java/com/github/cactuspuppy/mcmurder/game/MansionMurder.java
@@ -3,8 +3,15 @@ package com.github.cactuspuppy.mcmurder.game;
 import org.bukkit.Location;
 
 public class MansionMurder extends Murder {
+
     @Override
-    protected Location getRandomSpawnableLocation() {
+    protected Location getRandomPlayerSpawn() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    protected Location getRandomScrapSpawn() {
         //TODO
         return null;
     }
